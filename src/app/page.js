b5bb610978119f05a30969b87f3b325c501569cb
ehsan <ehsanmadadialengareqhi@gmail.com>
2023-03-26 16:40:41 +0330

    install tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-   <h1>Home</h1>
+   <h1 className="underline">Home</h1>
   )
 }
---
+++ src/pages/abut.js
@@ -0,0 +1,3 @@
+export default function Abut() {
+     return <div>About</div>
+   }
