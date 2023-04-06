c237f0b0fb6bae396c07ff3186c875742b699838
ehsan <ehsanmadadialengareqhi@gmail.com>
2023-04-06 14:14:04 +0330

    addLoadingEfect

---
+++ src/app/error.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import { useEffect } from 'react'
+export default function Error({error,reset}) {
+     useEffect((error)=>{console.log(error)},[error])
+  return (
+    <div className=' mx-auto border max-w-md p-5 text-center mt-12 space-y-5'>
+     <h1 className='text-red-300'>Somthimg Went Wrong</h1>
+     <button onClick={()=>reset()} className='hover:text-amber-500 bg-slate-700 p-3 px-5 rounded-3xl'>Reset</button>
+    </div>
+  )
+}
---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+
+///best sait for download spenerLoading is "Loading.io"
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+     <img className="h-92" src="Spinner.svg" alt="Loading.."/>
+    </div>
+  )
+}
